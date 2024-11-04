f75fdce444f35d22a68fabc97fc07964d9bb00e1
Creative-mindset21 <tobiawotoye03@gmail.com>
2024-11-04 21:08:38 +0000

    made the box and color code copyable

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-const colorPicker = document.getElementById("color-picker").value;
+//! GENERAL VARIABLES
+const colorPicker = document.getElementById("color-picker");
 const colorScheme = document.getElementById("color-scheme");
 const colorBtn = document.getElementById("color-scheme-btn");
 
+//! FUNCTION TO GENERATE COLOR SCHEME
 function generateColor() {
   const boxes = document.querySelectorAll(".box");
   const colorSchemeValue = colorScheme.value;
-  const color = colorPicker.replace("#", "");
+  const color = colorPicker.value.replace("#", "");
   const colorCode = document.querySelectorAll(".code");
   const count = 5;
-
-  let i = 0;
 
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${color}&mode=${colorSchemeValue}&count=${count}`
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.colors[0].hex.value);
-      for (box of boxes) {
+      for (let i = 0; i < count; i++) {
+        //! RENDER THE COLOR IN THE DOM
+        const box = boxes[i];
         box.style.background = data.colors[i].hex.value;
         colorCode[i].textContent = data.colors[i].hex.value;
-        i++;
+
+        box.addEventListener("click", () => {
+          copyToClipboard(data.colors[i].hex.value);
+        });
+
+        colorCode[i].addEventListener("click", () => {
+          copyToClipboard(data.colors[i].hex.value);
+        });
       }
     });
 }
+
+function copyToClipboard(color) {
+  navigator.clipboard
+    .writeText(color)
+    .then(() => {
+      alert(`Copied`);
+    })
+    .catch((err) => {
+      console.error("Could not copy text: ", err);
+    });
+}
+
+generateColor();
