32327526c953e356d9952ef72d24201ad5be6f77
Creative-mindset21 <tobiawotoye03@gmail.com>
2024-11-04 20:23:11 +0000

    created a function to generate the color scheme

---
+++ script.js
@@ -0,0 +1,27 @@
+const colorPicker = document.getElementById("color-picker").value;
+const colorScheme = document.getElementById("color-scheme");
+const colorBtn = document.getElementById("color-scheme-btn");
+
+function generateColor() {
+  const boxes = document.querySelectorAll(".box");
+  const colorSchemeValue = colorScheme.value;
+  const color = colorPicker.replace("#", "");
+  const count = 5;
+
+  let i = 0;
+  console.log(colorSchemeValue);
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${color}&mode=${colorSchemeValue}&count=${count}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.colors[0].hex.value);
+      for (box of boxes) {
+        box.style.background = data.colors[i].hex.value;
+        i++;
+      }
+    });
+}
+
+generateColor();
